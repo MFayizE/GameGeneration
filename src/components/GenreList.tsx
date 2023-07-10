0bf4497514c268eeacfd150b8e538920fc1038a6
Mohamed Fayis Elayodath <mfayize@gmail.com>
2023-07-10 23:21:00 +0530

    Show a spinner while fetching genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,18 @@
-import useGenres from "../hooks/useGenres"
+import { Spinner } from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
-    const  {genres} = useGenres()
+  const { genres, isLoading, error } = useGenres();
+
+  if (error) return null
+  if (isLoading) return <Spinner />;
   return (
     <ul>
-        {genres.map(genre => <li key={genre.id}>
-            {genre.name}
-        </li>)}
+      {genres.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
     </ul>
-  )
-}
+  );
+};
 
-export default GenreList
+export default GenreList;
