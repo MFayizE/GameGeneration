b130a3f1dde88cb8967d5ba8caa0b1d611782a33
Mohamed Fayis Elayodath <mfayize@gmail.com>
2023-07-10 23:29:08 +0530

    Create a generate data fetching hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,13 +2,14 @@ import { Spinner } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
-  const { genres, isLoading, error } = useGenres();
+  
+  const { data, isLoading, error } = useGenres()
 
   if (error) return null
   if (isLoading) return <Spinner />;
   return (
     <ul>
-      {genres.map((genre) => (
+      {data.map((genre) => (
         <li key={genre.id}>{genre.name}</li>
       ))}
     </ul>
