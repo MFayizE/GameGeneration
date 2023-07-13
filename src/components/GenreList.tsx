fddb1d66a4fe186c38e3e88a7934323fc8e01882
Mohamed Fayis Elayodath <mfayize@gmail.com>
2023-07-13 20:28:57 +0530

    Hightlight selected Genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,13 @@
-import { Button, HStack, Image, List, ListItem, Spinner,Text } from "@chakra-ui/react";
+import { Button, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre : Genre) => void
+  selectedGenre : Genre | null;
 }
 
-const GenreList = ({onSelectGenre} : Props) => {
+const GenreList = ({selectedGenre,onSelectGenre} : Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -21,7 +22,7 @@ const GenreList = ({onSelectGenre} : Props) => {
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Button onClick={() => onSelectGenre(genre)} variant='link' fontSize='lg'>
+            <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} variant='link' fontSize='lg'>
               {genre.name}
             </Button>
           </HStack>
